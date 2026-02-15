8868b40b0f97ba5a727700f2aef43dc3b8e4d22e
Ashish Kumar <66291666+Aashiskr@users.noreply.github.com>
2026-02-15 23:18:26 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (36.10%) - LeetSync

--- 67-add-binary/add-binary.py
+++ 67-add-binary/add-binary.py
@@ -1,19 +1,3 @@
-class Solution(object):
-    def addBinary(self, a, b):
-        ss = int(a) + int(b)
-        s = list(str(ss))
-        l = len(s)
-        
-        for i in range(l - 1, 0, -1):
-            d = int(s[i]) // 2
-            if d != 0:
-                r = int(s[i]) % 2
-                s[i] = str(r)
-                s[i-1] = str(int(s[i-1]) + d)
-
-        first_digit = int(s[0])
-        if first_digit >= 2:
-            s[0] = str(first_digit % 2)
-            s.insert(0, str(first_digit // 2))
-        
-        return "".join(s)
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        return bin(int(a,2)+int(b,2))[2:]
