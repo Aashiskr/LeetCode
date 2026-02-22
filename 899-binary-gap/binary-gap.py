9e079e730dadf431290739338d09f6efca62cdda
Ashish Kumar <66291666+Aashiskr@users.noreply.github.com>
2026-02-22 15:23:01 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (21.52%) - LeetSync

--- 899-binary-gap/binary-gap.py
+++ 899-binary-gap/binary-gap.py
@@ -8,20 +8,12 @@ class Solution(object):
         
         for i in range(l):
             if b[i] == '1':
-                # Agar pehla '1' mila hai
                 if a == 999:
                     a = i
-                # Agar agla '1' mila hai
                 else:
                     c = i
-                    
-                    # Distance check karo aur max update karo
                     if x < c - a:
                         x = c - a
-                    
-                    # MOST IMPORTANT FIX: 
-                    # Agli calculation ke liye 'a' ko hamesha naye '1' (yani c) par set karo
                     a = c
                     c = 999
-                    
         return x
