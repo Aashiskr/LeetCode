c8fc98b6f995f118e8e426d27f2768af33c8dafd
Ashish Kumar <ar6858439@gmail.com>
2026-03-08 08:24:55 +0530

    Time: 1 ms (39.71%) | Memory: 12.4 MB (91.18%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.py
+++ 2107-find-unique-binary-string/find-unique-binary-string.py
@@ -1,13 +1,20 @@
+# class Solution(object):
+#     def findDifferentBinaryString(self, nums):
+#         """
+#         :type nums: List[str]
+#         :rtype: str
+#         """
+#         n=nums[0]
+#         l=len(n)
+#         binary_list = [''.join(i) for i in itertools.product('01', repeat=l)]
+#         for i in binary_list:
+#             if i not in nums:
+#                 return i
 class Solution(object):
     def findDifferentBinaryString(self, nums):
-        """
-        :type nums: List[str]
-        :rtype: str
-        """
-        n=nums[0]
-        l=len(n)
-        binary_list = [''.join(i) for i in itertools.product('01', repeat=l)]
-        for i in binary_list:
-            if i not in nums:
-                return i
+        res = ""
+        for i in range(len(nums)):
+            # Flip the character at the diagonal position
+            res += "1" if nums[i][i] == "0" else "0"
+        return res
         
