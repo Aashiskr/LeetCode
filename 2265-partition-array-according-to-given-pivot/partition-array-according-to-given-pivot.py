ff053ba4b8e49b7e07b964e541a84a319c6a1475
Ashish Kumar <ar6858439@gmail.com>
2026-07-03 21:23:14 +0530

    Time: 87 ms (17.50%) | Memory: 30.1 MB (14.48%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
@@ -5,18 +5,30 @@ class Solution(object):
         :type pivot: int
         :rtype: List[int]
         """
-        less_than = []
-        equal_to = []
-        greater_than = []
+        n = len(nums)
+        ans = [0] * n
         
-        # Distribute elements into their respective buckets
-        for num in nums:
-            if num < pivot:
-                less_than.append(num)
-            elif num == pivot:
-                equal_to.append(num)
-            else:
-                greater_than.append(num)
+        # Two pointers: one starting from left (for elements < pivot)
+        # one starting from right (for elements > pivot)
+        left = 0
+        right = n - 1
+        
+        # Traverse from left to right for elements smaller than pivot
+        for i in range(n):
+            if nums[i] < pivot:
+                ans[left] = nums[i]
+                left += 1
                 
-        # Concatenate the lists to form the final partitioned array
-        return less_than + equal_to + greater_than
+        # Traverse from right to left for elements greater than pivot
+        # We read nums backwards to maintain the relative order when placing at the back
+        for i in range(n - 1, -1, -1):
+            if nums[i] > pivot:
+                ans[right] = nums[i]
+                right -= 1
+                
+        # Fill the remaining middle slots with the pivot
+        while left <= right:
+            ans[left] = pivot
+            left += 1
+            
+        return ans
